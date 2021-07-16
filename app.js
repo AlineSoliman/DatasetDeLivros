02dc410e281159bc792339ecfcb5e534f3686fd1
Aline Soliman <alineapreis@hotmail.com>
2021-07-15 23:45:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const livros = require('./database')
+
+//pegar o input
+
+// Se for sim, mostra as categorias disponíveis, pergunta qual categoria ela escolhe
+//se não, mostra todos os livros em ordem crescente pela quantidade de páginas
+
+const readline = require('readline-sync')
+
+const entradaInicial = readline.question('Deseja buscar um livro?S/N')
+
+if (entradaInicial.toLocaleUpperCase() === 'S') {
+  console.log('Essas são as categorias disponíveis:')
+  console.log('Suspense', '/Comédia Romantica', '/Ficção científica')
+
+  const entradaCategoria = readline.question('Qual categoria você escolhe:')
+
+  const retorno = livros.filter(livros => livros.categoria === entradaCategoria)
+
+  console.table(retorno)
+} else {
+  const livrosOrdenados = livros.sort((a, b) => a.paginas - b.paginas)
+  console.log('Esses são todos os livros disponíveis')
+  console.table(livrosOrdenados)
+}
---
+++ database.js
@@ -0,0 +1,49 @@
+const livros = [
+  {
+    id: 1,
+    nome: 'A Paciente Silenciosa',
+    autor: 'Alex Michaelides',
+    categoria: 'Suspense',
+    paginas: 353,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 2,
+    nome: 'A Mulher na Janela',
+    autor: 'A. J.Finn',
+    categoria: 'Suspense',
+    paginas: 378,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 3,
+    nome: 'Teto para dois',
+    autor: "Beth O'Leary",
+    categoria: 'Comédia romantica',
+    paginas: 400,
+    recomenda: false,
+    leu: true
+  },
+  {
+    id: 4,
+    nome: 'Objetos Cortantes',
+    autor: 'Gillian Flynn',
+    categoria: 'Drama',
+    paginas: 242,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 5,
+    nome: 'Flores para Argenon',
+    autor: 'Daniel Keyes',
+    categoria: 'Ficção científica',
+    paginas: 268,
+    recomenda: true,
+    leu: true
+  }
+]
+
+module.exports = livros
